f87db020246395d7f86e48a6215d8ef8d970b076
FatherOfCurses <colin.principe@gmail.com>
2022-08-21 21:45:22 -0400

    Update from TSLint to ESLint

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,20 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { paths } = require('./tsconfig.json').compilerOptions;
+
 module.exports = {
-  preset: 'jest-preset-angular',
+  preset: 'jest-preset-angular/presets/defaults-esm',
+  globals: {
+    'ts-jest': {
+      useESM: true,
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      tsconfig: '<rootDir>/tsconfig-esm.spec.json',
+    },
+  },
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(paths, { prefix: '<rootDir>' }),
+    tslib: 'tslib/tslib.es6.js',
+  },
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testPathIgnorePatterns: ['/node_modules'],
   verbose: true,
