a9f30b5e772e97691c1398c27fec7d75964f89f0
FatherOfCurses <colin.principe@gmail.com>
2026-02-10 11:27:54 -0500

    Cleanup before merge

--- src/app/services/userFileUploadService.ts
+++ src/app/services/userFileUploadService.ts
@@ -1,6 +1,6 @@
-import { ref, uploadBytes, getDownloadURL } from '@angular/fire/storage';
+import { ref, uploadBytes, getDownloadURL, FirebaseStorage } from '@angular/fire/storage';
 
-async function uploadUserFile(storage: Storage, uid: string, file: File) {
+async function uploadUserFile(storage: FirebaseStorage, uid: string, file: File) {
   const r = ref(storage, `users/${uid}/uploads/${Date.now()}_${file.name}`);
   await uploadBytes(r, file);
   return getDownloadURL(r);
